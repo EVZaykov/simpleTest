371e62f3da1e58e5ed4b605e54ba2402e3ea160f
EVZaykov <evzaykov@bk.ru>
2021-11-30 15:06:43 +0300

    add good report

--- src/test/java/Runners/RunCucumberTest.java
+++ src/test/java/Runners/RunCucumberTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = {"Steps"},
-        plugin = { "pretty", "html:target/cucumber-html-reports", "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm",
-        "json:target/cucumber-html-reports/cucumber.json","rerun:target/failed_scenarios.txt" }, features = "src/test/resources", tags = "@inWork")
+        plugin = { "pretty","io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"}, features = "src/test/resources", tags = "@inWork")
 public class RunCucumberTest {
 }
