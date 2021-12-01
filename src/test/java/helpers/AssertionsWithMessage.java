4f59ec5f5b1c309d0b324936d5ad52dc748e8938
EVZaykov <evzaykov@bk.ru>
2021-12-01 12:06:52 +0300

    add good report

---
+++ src/test/java/helpers/AssertionsWithMessage.java
@@ -0,0 +1,13 @@
+package helpers;
+
+import io.qameta.allure.Allure;
+import org.junit.Assert;
+
+public class AssertionsWithMessage {
+
+    public static void assertWithMessage(Object var1, Object var2) {
+        Allure.step("Expected Result = " + var1);
+        Allure.step("Actual Result = " + var2);
+        Assert.assertEquals(var1,var2);
+    }
+}
